7b86ebf987b2912074015b9e7157e0b7934f2230
Ilya_Karachun <Karachun_ilya94@mail.ru>
2022-08-14 16:04:41 +0300

    feat: hw4

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -60,7 +60,7 @@ function HW4() {
                     checked={checked}
                     onChangeChecked={setChecked}
                 >
-                    some text {/*// этот текст попадёт в children*/}
+                    Privetos {/*// этот текст попадёт в children*/}
                 </SuperCheckbox>
 
                 {/*// onChange тоже должен работать*/}
